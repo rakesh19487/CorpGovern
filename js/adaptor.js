20db4533996a96a34e4261ede5fbeaa3b1ef97ca
Rakesh Rengaraj <rakesh@ptotem.com>
2016-09-18 11:23:39 +0530

    ie changes

--- js/adaptor.js
+++ js/adaptor.js
@@ -220,11 +220,12 @@ function initBackpack() {
         $( ".mid-slide" ).empty();
         $(".top-content").append('<img src="img/how_adventure.png" class="backpack-icon" style="float:left">'+'<b class="backpack-txt">'+data[0].sub_header)
         $(".mid-slide").append(data[0].sub_contents);
-        // $('.mid-slide .mid-slide-div')
-        // .each(function(index){
-        //     var _this = this;
-        //     setTimeout( function(){ $(_this).fadeIn(); }, 200*index );
-        // });
+        $('.mid-slide .mid-slide-div')
+        .each(function(index){
+            var _this = this;
+            setTimeout( function(){ $(_this).fadeIn(); }, 200*index );
+        });
+        $('.back_pack_content').css('width','587px');
 
         $("#left_slide_btn").unbind('click').on('click', function() {
             var slideid = parseInt($(this).attr("slide_id"));
@@ -319,11 +320,12 @@ function getSubSlide(sub_slide_id,slide_id){
     $(".mid-slide" ).empty();
     $(".mid-slide").append(data[sub_slide_id-1].sub_contents);
     
-    // $('.mid-slide .mid-slide-div')
-    // .each(function(index){
-    //     var _this = this;
-    //     setTimeout( function(){ $(_this).fadeIn(); }, 2000*index );
-    // });
+    $('.mid-slide .mid-slide-div')
+    .each(function(index){
+        var _this = this;
+        setTimeout( function(){ $(_this).fadeIn(); }, 2000*index );
+    });
+    $('.back_pack_content').css('width','587px');
     $('.mid-slide ul li:empty').remove();
 }
 
