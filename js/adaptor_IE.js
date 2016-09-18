8e793c3de299c69e61f507f95046d0672778d5a5
Rakesh Rengaraj <rakesh@ptotem.com>
2016-09-18 10:40:46 +0530

    ie changes

--- js/adaptor_IE.js
+++ js/adaptor_IE.js
@@ -205,11 +205,12 @@ function initBackpack() {
         $( ".mid-slide" ).empty();
         $(".top-content").append('<img src="img/how_adventure.png" class="backpack-icon" style="float:left">'+'<b class="backpack-txt">'+data[0].sub_header)
         $(".mid-slide").append(data[0].sub_contents);
-        $('.mid-slide .mid-slide-div')
-        .each(function(index){
-            var _this = this;
-            setTimeout( function(){ $(_this).fadeIn(); }, 2000*index );
-        });        
+        $('.mid-slide .mid-slide-div').fadeIn();
+        // $('.mid-slide .mid-slide-div')
+        // .each(function(index){
+        //     var _this = this;
+        //     setTimeout( function(){ $(_this).fadeIn(); }, 2000*index );
+        // });        
 
         $("#left_slide_btn").unbind('click').on('click', function() {
             var slideid = parseInt($(this).attr("slide_id"));
